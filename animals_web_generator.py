f096620dd54f250ecb4c44c3b371ecb93415ed67
Sivaneswaran Sutharsan <s.sutharsan@outlook.de>
2025-03-20 19:16:22 +0100

    Generate a string with the animals’ data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,12 +8,12 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-for animal in animals_data:
-    print(f"Name: {animal.get('name', 'N/A')}")
-    print(f"Diet: {animal.get('characteristics', {}).get('diet', 'N/A')}")
-    if 'locations' in animal and animal['locations']:
-        print(f"Location: {animal['locations'][0]}")
-    print(f"Type: {animal.get('type', 'N/A')}")
+for animal_data in animals_data:
+    print(f"Name: {animal_data.get('name', 'N/A')}")
+    print(f"Diet: {animal_data.get('characteristics', {}).get('diet', 'N/A')}")
+    if 'locations' in animal_data and animal_data['locations']:
+        print(f"Location: {animal_data['locations'][0]}")
+    print(f"Type: {animal_data.get('type', 'N/A')}")
     print()  # Leerzeile für bessere Lesbarkeit
 
 file_path = 'animals_template.html'
@@ -22,3 +22,16 @@ with open(file_path, 'r') as file:
     file_content = file.read()
 
 print(file_content)
+
+output = ''  # define an empty string
+for animal_data in animals_data:
+    # append information to each string
+    output += f"Name: {animal_data['name']}\n"
+    output += f"Diet: {animal_data['characteristics']['diet']}\n"
+
+    if 'locations' in animal_data and animal_data['locations']:
+        output += f"Location: {animal_data['locations'][0]}\n"
+    output += f"Type: {animal_data.get('type', 'N/A')}\n"
+
+
+print(output)
