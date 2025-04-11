8cbabb7f764397be7244104cfa4309a36e1794d3
Sivaneswaran Sutharsan <s.sutharsan@outlook.de>
2025-04-11 18:05:10 +0200

    generate html form template

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,18 +5,36 @@ def generate_website(animal_name, animals_data):
     """
     Creates an HTML file based on the animal data.
     """
-    with open("animals.html", "w") as file:
-        file.write(f"<h1>Animals: {animal_name}</h1>")
+ # with open("animals.html", "w") as file:
+ #     file.write(f"<h1>Animals: {animal_name}</h1>")
+ #
+ #       if animals_data:  # If animals were found
+ #           for animal in animals_data:
+ #               file.write(f"<p>{animal['name']} - </p>")
+ #       else:
+ #           file.write(f"<h2>The animal '{animal_name}' doesn't exist.</h2>")
+ #
+ #   print("Website was successfully generated to the file animals.html.")
 
-        if animals_data:  # If animals were found
-            for animal in animals_data:
-                file.write(f"<p>{animal['name']} - </p>")
-        else:
-            file.write(f"<h2>The animal '{animal_name}' doesn't exist.</h2>")
+    #1 Animals Templates Html Datei einlesen
+    with open("animals_template.html", "r") as file:
+        contents = file.read()
 
-    print("Website was successfully generated to the file animals.html.")
+    #2 platzhalter string ersetzen durch gefundene Tiere /API output
+    new_html = ""
+    if animals_data:  # If animals were found
+        for animal in animals_data:
+            string_to_append = f"<p>{animal['name']} - </p>\n"
+            new_html = f"{new_html}{string_to_append}"
 
+    else:
+        new_html = f"<h2>The animal '{animal_name}' doesn't exist.</h2>"
 
+    modified_contents = contents.replace("__REPLACE_ANIMALS_INFO__", new_html)
+
+    #3 neuen html code in animals.html speichern
+    with open ("animals.html", "w") as output_file:
+        output_file.write(modified_contents)
 # Main program
 if __name__ == "__main__":
     animal_name = input("Please enter an animal: ")
